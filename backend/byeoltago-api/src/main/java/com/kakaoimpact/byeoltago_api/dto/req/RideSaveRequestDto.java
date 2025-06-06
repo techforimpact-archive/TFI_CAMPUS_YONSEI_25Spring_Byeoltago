6cf22bb8cb8bf2d8d57d4ae8695bf879d72d80ec
jiyoon <rhjiyoon@yonsei.ac.kr>
2025-06-06 16:27:33 +0900

    feat #34: add ride management APIs

---
+++ backend/byeoltago-api/src/main/java/com/kakaoimpact/byeoltago_api/dto/req/RideSaveRequestDto.java
@@ -0,0 +1,20 @@
+package com.kakaoimpact.byeoltago_api.dto.req;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+
+// RideSaveRequestDto.java
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class RideSaveRequestDto {
+    private String polyline;
+    private Timestamp startedAt;
+    private Timestamp endedAt;
+    private Boolean isShared;
+}
