37774f7f394573f2520a72e349ec4fb2a79dde5f
jiyoon <rhjiyoon@yonsei.ac.kr>
2025-06-12 12:40:11 +0900

    feat 35: CORS configuration 설정 및 login 연동

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -1,5 +1,10 @@
 import { API_BASE_URL } from './config.js';
 
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.querySelector(".login-box");
+  form.addEventListener("submit", handleLogin);
+});
+
 function handleLogin(event) {
     event.preventDefault();
 
@@ -10,6 +15,12 @@ function handleLogin(event) {
         alert("이메일과 비밀번호를 입력해주세요.");
         return;
     }
+
+    // 자전거 애니메이션 표시
+  const bike = document.getElementById("bike-animation");
+  if (bike) {
+    bike.style.display = "inline";
+  }
 
     fetch(`${API_BASE_URL}/auth/login`, {
       method: "POST",
@@ -24,10 +35,14 @@ function handleLogin(event) {
         return res.json();
       })
       .then(data => {
-        window.location.href = "mapdrive.html";    // 로그인 성공 시 이동
+        // 로그인 성공 후 1초 후 페이지 이동
+        setTimeout(() => {
+          window.location.href = "mapstart.html";
+        }, 1000);
       })
       .catch(err => {
         console.error("Login failed:", err);
         alert("로그인 정보가 올바르지 않습니다.");
+        if (bike) bike.style.display = "none"
       });
   }
