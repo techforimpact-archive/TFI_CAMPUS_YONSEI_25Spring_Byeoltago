27c4094c3be94826434c688ef465b109448cd2f4
Jiyoon <128347728+jiy0-0nv@users.noreply.github.com>
2025-06-13 10:48:45 +0900

    Update config.js
    https 연결 테스트

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,4 +1,5 @@
 // const API_BASE_URL = "http://localhost:8080/api/v1"; // 로컬 서버
-const API_BASE_URL = "http://43.203.245.224:8080/api/v1"; // 원격 서버
+// const API_BASE_URL = "http://43.203.245.224:8080/api/v1"; // 원격 서버
+const API_BASE_URL = "https://43.203.245.224:8080/api/v1"; // https
 
 export { API_BASE_URL };
